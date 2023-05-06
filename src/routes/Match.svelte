7cfdfdf2962066ca9e267d2ad62e6acc9858b5c6
liambr4 <liam.brown@dal.ca>
2023-05-06 14:17:03 -0300

    colrs, items

--- src/routes/Match.svelte
+++ src/routes/Match.svelte
@@ -3,6 +3,16 @@
 	export let match_id = '';
 	export let puuid = '';
 	let promise_match = match_details(match_id);
+	let items = ['item0', 'item1', 'item2', 'item3', 'item4', 'item5', 'item6'];
+	function getClassForMatch(win: boolean, pood: string) {
+		if (win == true) {
+			if (pood == puuid) return 'bg-green-400 border-2 border-green-400';
+			else return 'bg-green-300 border-2 border-green-300';
+		} else {
+			if (pood == puuid) return 'bg-red-400 border-2 border-red-400';
+			else return 'bg-red-300 border-2 border-red-300';
+		}
+	}
 </script>
 
 {#await promise_match then match}
@@ -36,13 +46,7 @@
 						{#each match.info.participants as participant, index}
 							{#if index < 5}
 								<div
-									class={`p-1 ${participant.win ? 'bg-green-300' : 'bg-red-300'} ${
-										participant.puuid == puuid
-											? 'border-2 border-blue-500'
-											: participant.win
-											? 'border-2 border-green-300'
-											: 'border-2 border-red-300'
-									}`}
+									class={`p-1 + ${getClassForMatch(participant.win, participant.puuid)}`}
 								>
 									<img
 										class="float-left pr-1 pb-0"
@@ -51,10 +55,30 @@
 										width="50"
 										height="50"
 									/>
-									<a
-										class={participant.puuid == puuid ? 'font-bold' : 'font-normal'}
-										href={`/?name=${participant.summonerName}`}>{participant.summonerName}</a
-									>
+									<div class="grid grid-cols-2">
+										<div class="grid grid-rows-2">
+											<a
+												class={participant.puuid == puuid ? 'font-bold' : 'font-normal'}
+												href={`/?name=${participant.summonerName}`}>{participant.summonerName}</a
+											>
+											<div>
+												{participant.kills + '/' + participant.deaths + '/' + participant.assists}
+											</div>
+										</div>
+										<div class="grid grid-cols-7">
+											{#each items as item}
+												{#if participant[item] != 0}
+													<img
+														class="float-left pr-1 pb-0"
+														alt="idk"
+														src={`http://ddragon.leagueoflegends.com/cdn/13.9.1/img/item/${participant[item]}.png`}
+														width="50"
+														height="50"
+													/>
+												{/if}
+											{/each}
+										</div>
+									</div>
 								</div>
 							{/if}
 						{/each}
@@ -65,13 +89,7 @@
 						{#each match.info.participants as participant, index}
 							{#if index > 4}
 								<div
-									class={`p-1 ${participant.win ? 'bg-green-300' : 'bg-red-300'} ${
-										participant.puuid == puuid
-											? 'border-2 border-blue-500'
-											: participant.win
-											? 'border-2 border-green-300'
-											: 'border-2 border-red-300'
-									}`}
+									class={`p-1 + ${getClassForMatch(participant.win, participant.puuid)}`}
 								>
 									<img
 										class="float-left pr-1 pb-0"
@@ -80,10 +98,30 @@
 										width="50"
 										height="50"
 									/>
-									<a
-										class={participant.puuid == puuid ? 'font-bold' : 'font-normal'}
-										href={`/?name=${participant.summonerName}`}>{participant.summonerName}</a
-									>
+									<div class="grid grid-cols-2">
+										<div class="grid grid-rows-2">
+											<a
+												class={participant.puuid == puuid ? 'font-bold' : 'font-normal'}
+												href={`/?name=${participant.summonerName}`}>{participant.summonerName}</a
+											>
+											<div>
+												{participant.kills + '/' + participant.deaths + '/' + participant.assists}
+											</div>
+										</div>
+										<div class="grid grid-cols-7">
+											{#each items as item}
+												{#if participant[item] != 0}
+													<img
+														class="float-left pr-1 pb-0"
+														alt="idk"
+														src={`http://ddragon.leagueoflegends.com/cdn/13.9.1/img/item/${participant[item]}.png`}
+														width="50"
+														height="50"
+													/>
+												{/if}
+											{/each}
+										</div>
+									</div>
 								</div>
 							{/if}
 						{/each}
